e99b5247f84b29045bcc852077b732a9d0b9285d
gqdw <aca_jingru@qq.com>
2015-09-04 14:17:00 +0800

    change for to query_set hostname__contains

--- cmdb/main/views.py
+++ cmdb/main/views.py
@@ -8,21 +8,25 @@ class Searchbox(forms.Form):
 	host = forms.CharField(max_length=30)
 
 def search( request ):
-	hosts = Host.objects.all()
+#	hosts = Host.objects.all()
 	if request.method == 'POST':
 		form = Searchbox(request.POST)
 		if form.is_valid():
 			cd = form.cleaned_data
 			print cd['host']
-			host_list = []
-			for h in hosts:
-				print h.hostname.find(cd['host'])
-				if h.hostname.find(cd['host']) != -1:
-				#if h.hostname == cd['host']:
-					host_list.append(h)
+			hosts = Host.objects.filter(hostname__contains=cd['host'] )
+			#hosts = Host.objects.get(hostname__iexact = cd['host'] )
+			print hosts
+#			host_list = []
+#			for h in hosts:
+#				print h.hostname.find(cd['host'])
+#				if h.hostname.find(cd['host']) != -1:
+#				#if h.hostname == cd['host']:
+#					host_list.append(h)
 	
 #			return HttpResponse('ok')
-			return render( request,'search.html',{ 'form':form ,'hosts':host_list})
+			return render( request,'search.html',{ 'form':form ,'hosts':hosts})
+			#return render( request,'search.html',{ 'form':form ,'hosts':host_list})
 	else:
 		form = Searchbox()
 	return 	render( request,'search.html',{ 'form':form })
