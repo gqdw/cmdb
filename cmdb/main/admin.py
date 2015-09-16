12b2cffa8f6daa53a348e99d8017a253d3f14b5a
gqdw <aca_jingru@qq.com>
2015-09-16 11:34:11 +0800

    modified:   db.sqlite3 	modified:   main/admin.py 	search_fields 支持外键有点问题.

--- cmdb/main/admin.py
+++ cmdb/main/admin.py
@@ -4,7 +4,9 @@ from .models import Hostgroup,Host,Region,Search,Os,Dbtype,Rds
 # Register your models here.
 class HostAdmin(admin.ModelAdmin):
 	list_display = ('hostname','eth0','eth1','group','os')
-	search_fields= ('hostname','eth0','eth1','group')
+	#search_fields= ['group']
+	search_fields= ['hostname','eth0']
+	#search_fields= ('hostname','eth0','eth1','group')
 	list_field = ('group',)
 #	filter_horizontal = ('Region',)
 
