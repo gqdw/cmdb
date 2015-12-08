f09f7bd8c2af51129471f8157e9126c54ca7d8b9
gqdw <aca_jingru@qq.com>
2015-12-08 10:30:33 +0800

    Changes to be committed: 	deleted:    cmdb/cmdb/__init__.pyc 	deleted:    cmdb/cmdb/settings.pyc 	deleted:    cmdb/cmdb/urls.pyc 	deleted:    cmdb/cmdb/wsgi.pyc 	modified:   cmdb/db.sqlite3 	deleted:    cmdb/main/__init__.pyc 	deleted:    cmdb/main/migrations/0001_initial.pyc 	deleted:    cmdb/main/migrations/0002_auto_20150903_0148.pyc 	deleted:    cmdb/main/migrations/0003_auto_20150903_0918.pyc 	deleted:    cmdb/main/migrations/0004_search.pyc 	deleted:    cmdb/main/migrations/0005_auto_20150905_1427.pyc 	deleted:    cmdb/main/migrations/0006_auto_20150906_0230.pyc 	deleted:    cmdb/main/migrations/0006_os.pyc 	deleted:    cmdb/main/migrations/0007_host_os.pyc 	deleted:    cmdb/main/migrations/__init__.pyc 	modified:   cmdb/main/models.py 	deleted:    cmdb/main/views.pyc

--- cmdb/main/models.py
+++ cmdb/main/models.py
@@ -2,38 +2,55 @@ from django.db import models
 
 # Create your models here.
 
+
 class Hostgroup(models.Model):
 	groupname = models.CharField(max_length=30)
+
 	def __unicode__(self):
 		return self.groupname
 
+
 class Region(models.Model):
-	regionname = models.CharField(max_length=30,default='cn-hangzhou')
+	regionname = models.CharField(max_length=30, default='cn-hangzhou')
+
 	def __unicode__(self):
 		return self.regionname
+
+
 class Search(models.Model):
-	times = models.IntegerField()	
+	times = models.IntegerField()
+
 	def __unicode__(self):
 		return str(self.times)
+
 	def add(self):
 		self.times += 1
 		self.save()
+
+
 class OsManager(models.Manager):
-	def create_os(self,name):
-		os=self.create(osname=name)
+
+	def create_os(self, name):
+		os = self.create(osname=name)
 		return os
+
+
 class Os(models.Model):
-	osname = models.CharField(max_length=10,default='Centos6',blank=True ,unique=True)
+	osname = models.CharField(max_length=10, default='Centos6', blank=True, unique=True)
+
 	def __unicode__(self):
 		return self.osname
+
 	objects = OsManager()
 	#@classmethod
 	#def create(cls,name):
 	#	os = cls(osname=name)
 	#	return os
 
+
 class Dbtype(models.Model):
-#	dbname = models.CharField(max_length=20,unique=True)
+# dbname = models.CharField(max_length=20,unique=True)
+
 	def setval(self,mem,con,iops):
 		self.mem = mem
 		self.connections = con
@@ -41,18 +58,23 @@ class Dbtype(models.Model):
 	mem = models.IntegerField(unique=True)
 	connections = models.IntegerField()
 	iops = models.IntegerField()
+
 	def __unicode__(self):
-		return "%sMB %i %i" % (self.mem,self.connections ,self.iops)
+		return "%sMB %i %i" % (self.mem, self.connections, self.iops)
+
 
 class Rds(models.Model):
 	group = models.ForeignKey(Hostgroup)
 	dns = models.CharField(max_length=50)
 	dbtype = models.ForeignKey(Dbtype)
+
 	def __unicode__(self):
 		return "%s %s" % (self.dns,self.dbtype)
 #type ,mem,connections,iops
+
 	Region = models.ForeignKey(Region,default=6)
 	
+
 class Host(models.Model):
 	CRegions = (
 		('hz','cn-hangzhou'),
@@ -64,24 +86,19 @@ class Host(models.Model):
 		('us1','us-west-1'),
 	)
 	group = models.ForeignKey(Hostgroup)
-	os = models.ForeignKey(Os,default=1)
-	hostname = models.CharField(max_length=30,unique=True)
+	os = models.ForeignKey(Os, default=1)
+	hostname = models.CharField(max_length=30, unique=True)
 	eth0 = models.GenericIPAddressField(unique=True)
-	eth1 = models.GenericIPAddressField( blank=True,null=True )
+	eth1 = models.GenericIPAddressField(blank=True, null=True )
 	beizhu = models.TextField(blank=True)
 #	Region = models.CharField(max_length=3,choices=CRegions)
-	Region = models.ForeignKey(Region,default=6)
+	Region = models.ForeignKey(Region, default=6)
 #	test = models.TextField()
 #	test2 = models.TextField()
-	def __unicode__(self):
-		return '%s %s %s %s' % (self.hostname,self.eth0, self.eth1 ,self.group )
 
+	def __unicode__(self):
+		return '%s %s %s %s' % (self.hostname, self.eth0, self.eth1, self.group)
 #	RegionID = 
 #	os = 
 #	cpu = 
 #	memory = 
-	
-
-
-	
-	
